72336deafa4b2c313ba8450059f90f0b63327e2e
Kartik <kartik.mathur10@gmail.com>
2024-01-07 21:44:50 +0530

    Question CF

---
+++ L-50-Graphs/OddOneOut.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		cout << (a ^ b ^ c) << endl;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
